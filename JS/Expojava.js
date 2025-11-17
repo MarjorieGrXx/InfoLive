a4ab91a7b6d2db4d95621b914ce5c96839927250
Lilcrep <danieljimenezmontiel2210@gmail.com>
2025-11-16 22:56:27 -0600

    idioma del menu
    se modifico todo el menu para admitir la opcion de cambiar idioma(por ahora solo en el menu)

--- JS/Expojava.js
+++ JS/Expojava.js
@@ -7,18 +7,6 @@ const textsToChange = document.querySelectorAll("[data-section]");
 console.log(textsToChange);
 /***************************/
 
-let tituloReceta = document.getElementById("tituloReceta")
-let textoReceta = document.getElementById("texto-receta")
-
-let imagenes = [...document.querySelectorAll(".imagen")]
-let ID = parseInt(photo.dataset.id)
-let bReceta = document.getElementById("miBoton")
-
-const equisCerrar = document.getElementById("equisCerrar")
-equisCerrar.addEventListener("click", (e) => {
-    grande.style.display = "none"
-})
-
 /******Para el cambio de idioma*******/
 
 let hizoClick = false;
@@ -67,6 +55,20 @@ if (!hizoClick) {
     });
 
 }
+
+let tituloReceta = document.getElementById("tituloReceta")
+let textoReceta = document.getElementById("texto-receta")
+
+let imagenes = [...document.querySelectorAll(".imagen")]
+let ID = parseInt(photo.dataset.id)
+let bReceta = document.getElementById("miBoton")
+
+const equisCerrar = document.getElementById("equisCerrar")
+equisCerrar.addEventListener("click", (e) => {
+    grande.style.display = "none"
+})
+
+
 
 function iniciarEventosDeImagenes() {
     let posicion = 0;
